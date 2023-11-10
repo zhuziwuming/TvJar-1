e1f9d40ffe632dd4f27f7c2d6229fc7c761d70a3
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-11-10 12:43:13 +0800

    Update WOGG.java

--- app/src/main/java/com/github/catvod/spider/WOGG.java
+++ app/src/main/java/com/github/catvod/spider/WOGG.java
@@ -163,7 +163,7 @@ public class WOGG extends Spider {
 		                }
 		    		if(down_url.contains("aliyundrive.com")){
 						vObj.put("vod_id", "push://" + down_url);	
-		            	vObj.put("vod_remarks", vod_remarks + "(VIP)");
+		            	vObj.put("vod_remarks", vObj.get("vod_id"));
 		            }
                     videos.put(vObj);  // 将修改过的 video 数据放入新的 JSONArray中					
                 }
