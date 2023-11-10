0d558a6398000bb098e189bfde75a6e13643d86b
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-11-10 15:26:19 +0800

    Update WOGG.java

--- app/src/main/java/com/github/catvod/spider/WOGG.java
+++ app/src/main/java/com/github/catvod/spider/WOGG.java
@@ -69,10 +69,10 @@ public class WOGG extends Spider {
 		if(down_url.contains("$$$")){
 			down_url = down_url.split("$$$")[0];
 		}
-		//if(down_url.contains("aliyundrive.com")){
+		if(down_url.contains("aliyundrive.com")){
 			vObj.put("vod_id", "push://" + down_url);	
 		        vObj.put("vod_remarks", vod_remarks + "(VIP)");
-		//}
+		}
                 videos.put(vObj);					
             }
             dataObject.put("list", videos);		
@@ -157,16 +157,18 @@ public class WOGG extends Spider {
             JSONArray jsonArray = dataObject.getJSONArray("list");
             JSONArray videos = new JSONArray();
             for (int i = 0; i < jsonArray.length(); i++) {
-		    		JSONObject vObj = jsonArray.getJSONObject(i);
-		    		String down_url = vObj.getString("vod_down_url");
-					String vod_remarks = vObj.getString("vod_remarks");
-		    			if(down_url.contains("$$$")){
-		    	            down_url = down_url.split("$$$")[0];
-		                }
-						vObj.put("vod_id", "push://" + down_url);	
-		            	vObj.put("vod_remarks", "push://" + down_url);
-                    videos.put(vObj);  // 将修改过的 video 数据放入新的 JSONArray中					
-                }
+		 JSONObject vObj = jsonArray.getJSONObject(i);
+		 String down_url = vObj.getString("vod_down_url");
+		 String vod_remarks = vObj.getString("vod_remarks");
+		 if(down_url.contains("$$$")){
+		    	down_url = down_url.split("$$$")[0];
+		 }
+		 if(down_url.contains("aliyundrive.com")){
+			vObj.put("vod_id", "push://" + down_url);	
+		        vObj.put("vod_remarks", vod_remarks + "(VIP)");
+		 }
+                 videos.put(vObj);  // 将修改过的 video 数据放入新的 JSONArray中					
+           }
            dataObject.put("list", videos);        
 	} catch (Exception e) {
             SpiderDebug.log(e);
