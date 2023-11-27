be6fe6fadfb9ee31216c89f98e8090e57ad7a3bc
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-11-27 11:36:26 +0800

    Update LocalFile.java

--- app/src/main/java/com/github/catvod/spider/LocalFile.java
+++ app/src/main/java/com/github/catvod/spider/LocalFile.java
@@ -115,9 +115,11 @@ public class LocalFile extends Spider {
         }
     }
 
-    public void init(Context context) {
+    public void init(Context context) throws Exception  {
+        }{
         LocalFile.super.init(context);
-    }
+    }catch (Exception ignored) {
+        }
 
     public String playerContent(String str, String str2, List<String> list) {
         try{
