a12204407ae40d844092d9282152a13765894d62
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-11-27 11:26:19 +0800

    Update AppYsV2.java

--- app/src/main/java/com/github/catvod/spider/AppYsV2.java
+++ app/src/main/java/com/github/catvod/spider/AppYsV2.java
@@ -29,11 +29,11 @@ import java.util.regex.Pattern;
 public class AppYsV2 extends Spider {
 
     @Override
-    public void init(Context context, String extend) {
+    public void init(Context context, String extend) throws Exception {
         super.init(context, extend);
         try {
             extInfos = extend.split("###");
-        } catch (Throwable th) {
+        } catch (Exception ignored) {
         }
     }
 
