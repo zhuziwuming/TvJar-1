d42ec5e17bd9fd291f8b2dda65a45a284247cb8e
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-04-26 19:16:17 +0800

    测试

---
+++ app/src/main/java/com/github/catvod/spider/Pptv.java
@@ -0,0 +1,195 @@
+package com.github.catvod.spider;
+
+import android.content.Context;
+import android.util.Base64;
+
+import com.github.catvod.bean.Class;
+import com.github.catvod.bean.Result;
+import com.github.catvod.bean.Vod;
+import com.github.catvod.crawler.Spider;
+import com.github.catvod.net.OkHttp;
+import com.github.catvod.utils.AES;
+import com.github.catvod.utils.Util;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Pptv extends Spider {
+
+    private static String siteUrl = "https://www.pptv06.com";
+
+    private Map<String, String> getHeader() {
+        Map<String, String> header = new HashMap<>();
+        header.put("User-Agent", Util.CHROME);
+        return header;
+    }
+
+    @Override
+    public void init(Context context, String extend) throws Exception {
+        super.init(context, extend);
+        if (!extend.isEmpty()) {
+            this.siteUrl = extend;
+        }
+    }
+
+    @Override
+    public String homeContent(boolean filter) throws Exception {
+        List<Class> classes = new ArrayList<>();
+        List<String> typeIds = Arrays.asList("1", "2", "3", "4", "5");
+        List<String> typeNames = Arrays.asList("电影", "剧集", "综艺", "动漫", "短剧");
+        for (int i = 0; i < typeIds.size(); i++)
+            classes.add(new Class(typeIds.get(i), typeNames.get(i)));
+        String f = "{\"1\": [{\"key\": \"area\", \"name\": \"地区\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"大陆\", \"v\": \"大陆\"}, {\"n\": \"香港\", \"v\": \"香港\"}, {\"n\": \"台湾\", \"v\": \"台湾\"}, {\"n\": \"美国\", \"v\": \"美国\"}, {\"n\": \"法国\", \"v\": \"法国\"}, {\"n\": \"英国\", \"v\": \"英国\"}, {\"n\": \"日本\", \"v\": \"日本\"}, {\"n\": \"韩国\", \"v\": \"韩国\"}, {\"n\": \"德国\", \"v\": \"德国\"}, {\"n\": \"泰国\", \"v\": \"泰国\"}, {\"n\": \"印度\", \"v\": \"印度\"}, {\"n\": \"意大利\", \"v\": \"意大利\"}, {\"n\": \"西班牙\", \"v\": \"西班牙\"}, {\"n\": \"加拿大\", \"v\": \"加拿大\"}, {\"n\": \"其他\", \"v\": \"其他\"}]}, {\"key\": \"year\", \"name\": \"年份\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"2024\", \"v\": \"2024\"}, {\"n\": \"2023\", \"v\": \"2023\"}, {\"n\": \"2022\", \"v\": \"2022\"}, {\"n\": \"2021\", \"v\": \"2021\"}, {\"n\": \"2020\", \"v\": \"2020\"}, {\"n\": \"2019\", \"v\": \"2019\"}, {\"n\": \"2018\", \"v\": \"2018\"}, {\"n\": \"2017\", \"v\": \"2017\"}, {\"n\": \"2016\", \"v\": \"2016\"}, {\"n\": \"2015\", \"v\": \"2015\"}, {\"n\": \"2014\", \"v\": \"2014\"}, {\"n\": \"2013\", \"v\": \"2013\"}, {\"n\": \"2012\", \"v\": \"2012\"}, {\"n\": \"2011\", \"v\": \"2011\"}, {\"n\": \"2010\", \"v\": \"2010\"}]}, {\"key\": \"by\", \"name\": \"排序\", \"value\": [{\"n\": \"时间\", \"v\": \"time\"}, {\"n\": \"人气\", \"v\": \"hits\"}, {\"n\": \"评分\", \"v\": \"score\"}]}],\"2\": [{\"key\": \"area\", \"name\": \"地区\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"大陆\", \"v\": \"大陆\"}, {\"n\": \"台湾\", \"v\": \"台湾\"}, {\"n\": \"香港\", \"v\": \"香港\"}, {\"n\": \"韩国\", \"v\": \"韩国\"}, {\"n\": \"日本\", \"v\": \"日本\"}, {\"n\": \"美国\", \"v\": \"美国\"}, {\"n\": \"泰国\", \"v\": \"泰国\"}, {\"n\": \"英国\", \"v\": \"英国\"}, {\"n\": \"英国\", \"v\": \"英国\"}, {\"n\": \"其他\", \"v\": \"其他\"}]}, {\"key\": \"year\", \"name\": \"年份\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"2024\", \"v\": \"2024\"}, {\"n\": \"2023\", \"v\": \"2023\"}, {\"n\": \"2022\", \"v\": \"2022\"}, {\"n\": \"2021\", \"v\": \"2021\"}, {\"n\": \"2020\", \"v\": \"2020\"}, {\"n\": \"2019\", \"v\": \"2019\"}, {\"n\": \"2018\", \"v\": \"2018\"}, {\"n\": \"2017\", \"v\": \"2017\"}, {\"n\": \"2016\", \"v\": \"2016\"}, {\"n\": \"2015\", \"v\": \"2015\"}, {\"n\": \"2014\", \"v\": \"2014\"}, {\"n\": \"2013\", \"v\": \"2013\"}, {\"n\": \"2012\", \"v\": \"2012\"}, {\"n\": \"2011\", \"v\": \"2011\"}, {\"n\": \"2010\", \"v\": \"2010\"}, {\"n\": \"2009\", \"v\": \"2009\"}, {\"n\": \"2008\", \"v\": \"2008\"}, {\"n\": \"2006\", \"v\": \"2006\"}, {\"n\": \"2005\", \"v\": \"2005\"}, {\"n\": \"2004\", \"v\": \"2004\"}]}, {\"key\": \"by\", \"name\": \"排序\", \"value\": [{\"n\": \"时间\", \"v\": \"time\"}, {\"n\": \"人气\", \"v\": \"hits\"}, {\"n\": \"评分\", \"v\": \"score\"}]}],\"4\": [{\"key\": \"area\", \"name\": \"地区\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"大陆\", \"v\": \"大陆\"}, {\"n\": \"日本\", \"v\": \"日本\"}, {\"n\": \"美国\", \"v\": \"美国\"},]}, {\"key\": \"year\", \"name\": \"年份\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"2024\", \"v\": \"2024\"}, {\"n\": \"2023\", \"v\": \"2023\"}, {\"n\": \"2022\", \"v\": \"2022\"}, {\"n\": \"2021\", \"v\": \"2021\"}, {\"n\": \"2020\", \"v\": \"2020\"}, {\"n\": \"2019\", \"v\": \"2019\"}, {\"n\": \"2018\", \"v\": \"2018\"}, {\"n\": \"2017\", \"v\": \"2017\"}, {\"n\": \"2016\", \"v\": \"2016\"}, {\"n\": \"2015\", \"v\": \"2015\"}, {\"n\": \"2014\", \"v\": \"2014\"}, {\"n\": \"2013\", \"v\": \"2013\"}, {\"n\": \"2012\", \"v\": \"2012\"}, {\"n\": \"2011\", \"v\": \"2011\"}, {\"n\": \"2010\", \"v\": \"2010\"}, {\"n\": \"2009\", \"v\": \"2009\"}, {\"n\": \"2008\", \"v\": \"2008\"}, {\"n\": \"2006\", \"v\": \"2006\"}, {\"n\": \"2005\", \"v\": \"2005\"}, {\"n\": \"2004\", \"v\": \"2004\"}]}, {\"key\": \"by\", \"name\": \"排序\", \"value\": [{\"n\": \"时间\", \"v\": \"time\"}, {\"n\": \"人气\", \"v\": \"hits\"}, {\"n\": \"评分\", \"v\": \"score\"}]}], \"3\": [{\"key\": \"area\", \"name\": \"地区\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"大陆\", \"v\": \"大陆\"}, {\"n\": \"台湾\", \"v\": \"台湾\"}, {\"n\": \"香港\", \"v\": \"香港\"}, {\"n\": \"韩国\", \"v\": \"韩国\"}, {\"n\": \"日本\", \"v\": \"日本\"}, {\"n\": \"美国\", \"v\": \"美国\"}]}, {\"key\": \"year\", \"name\": \"年份\", \"value\": [{\"n\": \"全部\", \"v\": \"\"}, {\"n\": \"2024\", \"v\": \"2024\"}, {\"n\": \"2023\", \"v\": \"2023\"}, {\"n\": \"2022\", \"v\": \"2022\"}, {\"n\": \"2021\", \"v\": \"2021\"}, {\"n\": \"2020\", \"v\": \"2020\"}, {\"n\": \"2019\", \"v\": \"2019\"}, {\"n\": \"2018\", \"v\": \"2018\"}, {\"n\": \"2017\", \"v\": \"2017\"}, {\"n\": \"2016\", \"v\": \"2016\"}, {\"n\": \"2015\", \"v\": \"2015\"}, {\"n\": \"2014\", \"v\": \"2014\"}, {\"n\": \"2013\", \"v\": \"2013\"}, {\"n\": \"2012\", \"v\": \"2012\"}, {\"n\": \"2011\", \"v\": \"2011\"}, {\"n\": \"2010\", \"v\": \"2010\"}, {\"n\": \"2009\", \"v\": \"2009\"}, {\"n\": \"2008\", \"v\": \"2008\"}, {\"n\": \"2006\", \"v\": \"2006\"}, {\"n\": \"2005\", \"v\": \"2005\"}, {\"n\": \"2004\", \"v\": \"2004\"}]}, {\"key\": \"by\", \"name\": \"排序\", \"value\": [{\"n\": \"时间\", \"v\": \"time\"}, {\"n\": \"人气\", \"v\": \"hits\"}, {\"n\": \"评分\", \"v\": \"score\"}]}]}";
+        JSONObject filterConfig = new JSONObject(f);
+        Document doc = Jsoup.parse(OkHttp.string(siteUrl, getHeader()));
+        List<Vod> list = new ArrayList<>();
+        for (Element li : doc.select(".module-items .module-item")) {
+            String vid = siteUrl + li.attr("href");
+            String name = li.attr("title");
+            String pic = li.select("img").attr("data-original");
+            String remark = li.select(".module-item-note").text();
+            list.add(new Vod(vid, name, pic, remark));
+        }
+        return Result.string(classes, list, filterConfig);
+    }
+
+    @Override
+    public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
+        HashMap<String, String> ext = new HashMap<>();
+        if (extend != null && extend.size() > 0)ext.putAll(extend);
+        String cateId = ext.get("cateId") == null ? tid : ext.get("cateId");
+        String area = ext.get("area") == null ? "" : ext.get("area");
+        String year = ext.get("year") == null ? "" : ext.get("year");
+        String by = ext.get("by") == null ? "" : ext.get("by");
+        String classType = ext.get("class") == null ? "" : ext.get("class");
+        String vodType = siteUrl + Jsoup.parse(OkHttp.string(siteUrl, getHeader())).select("a.links").eq(1).attr("href");
+        String vodShow = Jsoup.parse(OkHttp.string(vodType, getHeader())).select("a.module-heading-tab-link") .attr("href").split("/")[1];
+        String cateUrl = siteUrl + String.format("/" + vodShow + "/%s-%s-%s-%s-----%s---%s/", cateId, area, by, classType, pg, year);
+        Document doc = Jsoup.parse(OkHttp.string(cateUrl, getHeader()));
+        List<Vod> list = new ArrayList<>();
+        for (Element li : doc.select(".module .module-item")) {
+            String vid = siteUrl + li.attr("href");
+            String name = li.attr("title");
+            String pic = li.select("img").attr("data-original");
+            String remark = li.select("[class=module-item-note]").text();
+            list.add(new Vod(vid, name, pic, remark));
+        }
+        return Result.string(list);
+    }
+
+    @Override
+    public String detailContent(List<String> ids) throws Exception {
+        String detailUrl = ids.get(0);
+        String content = OkHttp.string(detailUrl, getHeader());
+        Document doc = Jsoup.parse(content);
+        Elements sources = doc.select(".module-play-list"); // 线路
+        Elements circuits = doc.select("div.module-tab-items-box .module-tab-item"); // 播放源标题
+        StringBuilder vod_play_url = new StringBuilder(); // 线路/播放源 里面的各集的播放页面链接
+        StringBuilder vod_play_from = new StringBuilder(); // 线路 / 播放源标题        
+        for (int i = 0; i < sources.size(); i++) {
+            String spanText = circuits.get(i).select("span").text();
+            String smallText = circuits.get(i).select("small").text();
+		if (spanText.contains("GS线路") || spanText.contains("HH线路") || spanText.contains("WJ线路") || spanText.contains("SN线路") || spanText.contains("HN线路")) {
+                continue;
+            }
+            String playFromText = spanText + "(共" + smallText + "集)";
+            vod_play_from.append(playFromText).append("$$$");            
+            Elements aElementArray = sources.get(i).select("a");
+            for (int j = 0; j < aElementArray.size(); j++) {
+                Element a = aElementArray.get(j);
+                String href = siteUrl + a.attr("href");
+                String text = a.text();
+                vod_play_url.append(text).append("$").append(href);
+                boolean notLastEpisode = j < aElementArray.size() - 1;
+                vod_play_url.append(notLastEpisode ? "#" : "$$$");
+            }
+        }
+
+        String title = doc.select(".module-info-heading")
+                .get(0)
+                .getElementsByTag("h1").text();
+        String pic = doc.select(".module-info-poster")
+                .get(0)
+                .select("img")
+                .attr("data-original");
+        Elements elements = doc.select(".module-info-heading").select(".module-info-tag-link");
+        String classifyName = ""; // 影片类型
+        String year = ""; // 影片年代
+        String area = ""; // 地区
+        if (elements.size() >= 3) {
+            classifyName = elements.get(2).select("a").text();
+            year = elements.get(0).select("a").text();
+            area = elements.get(1).select("a").text();
+        }
+        Elements moduleInfoItems = doc.select(".module-info-items").select(".module-info-item");
+        String remark = ""; // 备注
+        String actor = ""; // 演员
+        String director = ""; // 导演
+        if (moduleInfoItems.size() >= 6) {
+            remark = moduleInfoItems.get(4).select(".module-info-item-content").text();
+            actor = moduleInfoItems.get(2).select("a").text();
+            director = moduleInfoItems.get(1).select("a").text();
+        }
+        String brief = doc.select(".module-info-introduction-content").text();
+        Vod vod = new Vod();
+        vod.setVodId(ids.get(0));
+        vod.setVodPic(pic);
+        vod.setVodYear(year);
+        vod.setVodName(title);
+        vod.setVodArea(area);
+        vod.setVodActor(actor);
+        vod.setVodRemarks(remark);
+        vod.setVodContent(brief);
+        vod.setVodDirector(director);
+        vod.setTypeName(classifyName);
+        vod.setVodPlayFrom(vod_play_from.toString());
+        vod.setVodPlayUrl(vod_play_url.toString());
+        return Result.string(vod);
+    }
+
+    @Override
+    public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
+        return Result.get().url(id).parse().header(getHeader()).string();
+    }
+
+    @Override
+    public String searchContent(String key, boolean quick) throws Exception {
+        String searchUrl = siteUrl + "/index.php/ajax/suggest.html?mid=1&wd=" + URLEncoder.encode(key);
+        String content = OkHttp.string(searchUrl, getHeader());
+        JSONArray lists = new JSONObject(content).getJSONArray("list");
+        List<Vod> list = new ArrayList<>();
+        for (int i = 0; i < lists.length(); i++) {
+            JSONObject item = lists.getJSONObject(i);
+            String vid = siteUrl + "/voddetail/" + item.getInt("id") + ".html";
+            String name = item.getString("name");
+            String pic = item.getString("pic");
+            list.add(new Vod(vid, name, pic));
+        }
+        return Result.string(list);
+    }
+}
