1dccf9f5d87cb657b87e8156f3544bdeb824896a
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2024-07-13 21:45:04 +0800

    更新 QQ.java

--- app/src/main/java/com/github/catvod/spider/QQ.java
+++ app/src/main/java/com/github/catvod/spider/QQ.java
@@ -214,50 +214,6 @@ public class QQ extends Spider {
             if (filter) {
                 jSONObject.put("filters", this.extjson);
             }
-
-			try {
-				String htmlContent = OkHttpUtil.string("https://v.qq.com/", getHeaders("https://v.qq.com/"));
-				Pattern pattern = Pattern.compile("window\\.__INITIAL_STATE__\\s*=\\s*([\\s\\S]*?)<\\/script>", Pattern.DOTALL);  
-				Matcher matcher = pattern.matcher(htmlContent);  
-				JSONArray listArray = new JSONArray();
-				if (matcher.find()) {
-					String initialStateContent = matcher.group(1).trim();
-					Toast.makeText(mContext, initialStateContent, Toast.LENGTH_SHORT).show();
-					//initialStateContent = URLDecoder.decode(initialStateContent, "UTF-8");
-					JSONObject jsonObject1 = new JSONObject(initialStateContent);
-					// 逐层访问到目标数据  
-					JSONObject storeModulesData = jsonObject1.getJSONObject("storeModulesData");  
-					JSONObject channelsModulesMap = storeModulesData.getJSONObject("channelsModulesMap");  
-					JSONObject choice = channelsModulesMap.getJSONObject("choice");  
-					JSONArray cardListData = choice.getJSONArray("cardListData");  
-						JSONObject firstCardListData = cardListData.getJSONObject(0); // 注意这里使用 getJSONObject 而不是索引+方括号  
-						// 获取children_list  
-						JSONObject childrenList = firstCardListData.getJSONObject("children_list");  
-						// 获取list  
-						JSONObject list = childrenList.getJSONObject("list");  
-						// 最后获取cards  
-						JSONArray cards = list.getJSONArray("cards");
-					for (int i = 0; i < cards.length(); i++) {
-						JSONObject params = cards.getJSONObject(i).getJSONObject("params"); // 获取JSONArray中的JSONObject
-						String title = params.getString("title"); 
-						String picurl = params.getString("image_url_vertical");
-						String cid = params.getString("cid");
-						String remarks = params.getString("stitle_pc");
-						JSONObject jSONObject2 = new JSONObject();
-						jSONObject2.put("vod_id", cid);
-						jSONObject2.put("vod_name", title);
-						jSONObject2.put("vod_pic", picurl);
-						jSONObject2.put("vod_remarks", remarks);
-						listArray.put(jSONObject2);
-					}
-				}else{
-					Toast.makeText(mContext, "没有匹配到", Toast.LENGTH_SHORT).show();
-				}
-					jSONObject.put("list", listArray);
-			} catch (Exception e) {
-				SpiderDebug.log(e);
-				Toast.makeText(mContext, "json异常", Toast.LENGTH_SHORT).show();
-			}
             return jSONObject.toString();
         } catch (Exception e2) {
             SpiderDebug.log(e2);
@@ -308,7 +264,8 @@ public class QQ extends Spider {
 		} catch (Exception e) {
 			SpiderDebug.log(e);
 			return "";
-		}             
+		}
+		return "";		
     }
 
     public String join(@NonNull CharSequence charSequence, @NonNull Iterable iterable) {
